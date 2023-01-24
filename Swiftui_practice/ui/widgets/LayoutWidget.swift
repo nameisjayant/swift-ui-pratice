03fc1a3df5ba9e26decaad4a62570ed5b8e26fad
nameisjayant <jayntkumar99@gmail.com>
2023-01-24 13:38:23 +0530

    layout arrangement in swift ui

--- Swiftui_practice/ui/widgets/LayoutWidget.swift
+++ Swiftui_practice/ui/widgets/LayoutWidget.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct LayoutWidget: View {
+    @State var counter = 0
     var body: some View {
-        VStack(alignment : .center){
-            ScrollView{
-                Text("Text one")
-                Text("Text two")
-                Text("Text three")
+        
+        
+//        VStack(alignment : .leading){
+//            Spacer()
+//            Text("Text")
+//            Spacer()
+//            Text("Text two")
+//            Spacer()
+//            Text("Text three")
+//            Spacer()
+//
+//        }.padding()
+//        .frame(maxWidth: .infinity,alignment: .leading)
+//        HStack{
+//            Text("One")
+//            Spacer()
+//            Text("two")
+//            Spacer()
+//            Text("three")
+//        }.padding()
+//            .frame(maxHeight: .infinity,
+//                   alignment: .bottom)
+       
+        NavigationStack{
+            ZStack{
+                Text("\(counter)")
+                    .font(.largeTitle)
+                VStack{
+                    Spacer()
+                    Button{
+                       counter =  counter + 1
+                    }label: {
+                                          Image(systemName: "plus.circle.fill")
+                                              .resizable()
+                                              .frame(width: 70, height: 70)
+                                      }
+                                      .padding([.trailing, .bottom], 10)
+                }.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .trailing)
+
             }
-        }.frame(maxWidth: .infinity,maxHeight: .infinity)
-        .background(.red)
+            .frame(maxWidth: .infinity,maxHeight: .infinity,alignment : .topLeading)
+        }
+            
+        
     }
 }
 
